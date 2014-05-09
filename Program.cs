9502c94e1313e122189a1b62017fa0a5c3884226
uc12706501 <476362348@qq.com>
2014-05-09 21:32:10 +0800

    First Commit of project.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExpertChooseCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HelloWorld");
+            Console.ReadKey();
+        }
+    }
+}
