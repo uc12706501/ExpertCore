8cd35f5190e90eda63757768afc053b51c97cb38
uc12706501 <uc12706501@gmail.com>
2014-10-14 20:43:32 +0800

    创建模型 1. 添加了Project模型，表示一个项目的信息 2. 添加了Expert模型，表示一个专家的信息

---
+++ ExpertChooseSystem/model/Expert.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExpertChooseSystem.Model
+{
+    class Expert
+    {
+        public String Field { get; set; }
+        public String Name { get; set; }
+
+        #region 排序属性
+
+        //基本情况
+        public int Age { get; set; }
+        public double TitleRank { get; set; }
+        public double DegreeRank { get; set; }
+
+        //专业能力指标
+        public int H { get; set; }
+        public double ProjectRank { get; set; }
+        public double AwardRank { get; set; }
+        public double PatentRank { get; set; }
+
+        //道德修养指标
+        public int AcademicMoralityCount { get; set; }
+        public int AttitudeRank { get; set; }
+        public int StyleOfWorkRank { get; set; }
+
+        //评价业绩指标
+        public double ParticipationRate { get; set; }
+        public double DisperseRate { get; set; }
+        public double HitRate { get; set; }
+        public double SuccessRate { get; set; }
+
+        #endregion
+    }
+}
---
+++ ExpertChooseSystem/model/Project.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExpertChooseSystem.Model
+{
+    class Project
+    {
+        public String Field { get; set; }
+        public String Name { get; set; }
+        public List<Expert> ReviewExperts { get; set; }
+    }
+}
